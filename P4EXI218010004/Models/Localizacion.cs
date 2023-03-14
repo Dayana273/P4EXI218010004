3ce6ffaff59d0926bc763a2721d42984c9a20def
Dayana <Dayana@DESKTOP-BMET23Q>
2023-03-13 19:49:23 -0600

    Agregar archivos de proyecto.

---
+++ P4EXI218010004/Models/Localizacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace P4EXI218010004.Models
+{
+    public class Localizacion
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id {  get; set; }    
+
+        public Double Latitud  { get; set; }
+
+        public Double Longitud { get; set; }
+
+        [MaxLength(200)]
+        public  string descripcion_larga { get; set; }
+
+        [MaxLength(60)]
+        public  string descripcion_corta { get; set; }
+    }
+}
